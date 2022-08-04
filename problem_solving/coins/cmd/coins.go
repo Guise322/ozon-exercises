693501472df9d36c831b379281feb021733dc3b0
dmitry's notebook <guise322@ya.ru>
2022-08-04 11:44:22 +0300

    Solve the problem 5

--- problem_solving/coins/cmd/coins.go
+++ problem_solving/coins/cmd/coins.go
@@ -1,11 +1,27 @@
+/*
+You have n coins and you want to build a staircase with these coins. The staircase consists of k rows where the ith row
+has exactly i coins. The last row of the staircase may be incomplete.
+
+Given the integer n, return the number of complete rows of the staircase you will build.
+*/
 package main
 
 import "fmt"
 
 func main() {
-	fmt.Printf("%v", arrangeCoins(6))
+	fmt.Printf("%v", arrangeCoins(5))
 }
 
 func arrangeCoins(n int) int {
-	return 4
+	cnt := 1
+	var sum int
+	for {
+		sum += cnt
+		if n < sum {
+			return cnt - 1
+		} else if n == sum {
+			return cnt
+		}
+		cnt += 1
+	}
 }
